eb3d139d3b6ac836d36b531669fba4e8079a0841
Bernat Ibáñez Martínez <personal@bibanez.xyz>
2025-12-29 16:21:55 +0100

    Distinguish line from block comments

--- grammar.js
+++ grammar.js
@@ -1249,18 +1249,18 @@ module.exports = grammar({
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     // from: https://github.com/tree-sitter/tree-sitter-c/blob/master/grammar.js
-    comment: $ => token(choice(
-      seq('//', /.*/),
+    lineComment: $ => token(seq('//', /.*/)),
+    blockComment: $ => token(
       seq(
         '/*',
         /[^*]*\*+([^/*][^*]*\*+)*/,
         '/',
-      ),
-    )),
+      )
+    ),
 
   },
 
-  extras: $ => [/\s/, $.comment],
+  extras: $ => [/\s/, $.lineComment, $.blockComment],
 
   // tree-sitter has automatic keyword extraction
   word: $ => $.identifier,
